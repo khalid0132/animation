a2a16e3f6314f5050b2823622b350013b57b0f9a
Khalid Saifullah <khalid0132@gmail.com>
2020-12-06 01:13:21 +0100

    simpleAnimation

---
+++ js/custom.js
@@ -0,0 +1,33 @@
+$(function(){
+   $(".btn-1").click(function(){
+    $(".khalid").animate({
+    left:'350px'
+    },1000);  
+   
+    $(".khalid").animate({
+    top:'350px'
+    },1500);
+    
+    $(".khalid").animate({
+    left:'0'
+    },2000);
+       
+    $(".khalid").animate({
+    top:'0'
+    },2500);
+       
+   });
+    
+    
+    /* $(".btn-1").click(function(){
+        $(".khalid").animate({
+        left:'360px'
+        },1000);
+        $(".khalid").animate({
+        left:'0'
+        },1000);
+    });
+    */
+  
+
+});
